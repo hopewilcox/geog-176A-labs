c70d3e13def06684e95ed9628c30d517cce6cccb
hopewilcox <hopewilcox@ucsb.edu>
2020-09-01 19:47:45 -0700

    labs commit

---
+++ R/utils.R
@@ -0,0 +1,43 @@
+map_nearest_state = function(name){
+
+
+states = USAboundaries::us_states()
+
+
+state.of.interest = name
+
+soi = filter(states, state_name == state.of.interest)
+
+adjoining = st_filter(states, soi, .predicate = st_touches)
+
+
+closest = st_make_grid(soi, n = 70, square = F) %>%
+  st_centroid() %>%
+  st_sf() %>%
+  st_join(adjoining, join = st_nearest_feature)
+
+vor = closest %>%
+  st_union() %>%
+  st_voronoi() %>%
+  st_cast() %>%
+  st_sf() %>%
+  st_join(closest) %>%
+  summarise() %>%
+  st_intersection(soi)
+
+
+leaflet() %>%
+  addProviderTiles(providers$CartoDB) %>%
+  addCircles(data = st_transform(vor, 4326),
+             radius = 1, color = ~colorFactor("viridis", state_name)(state_name)) %>%
+  addPolygons(data = st_transform(soi, 4326),
+              fillColor = "transparent",
+              color = "black") %>%
+  addPolygons(data = st_transform(adjoining, 4326),
+              fillColor = ~colorFactor("viridis", state_name)(state_name), col = "transparent") %>%
+  addLayersControl(overlayGroups = c("SOI"))
+
+
+}
+
+
